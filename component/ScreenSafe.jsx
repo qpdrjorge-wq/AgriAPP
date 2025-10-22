be88f8405f52f6afffd61619b1fea0e72dee6fe9
Sir Tapos <qpdrjorge@tip.edu.ph>
2025-10-22 22:29:26 +0800

    Add files via upload

---
+++ component/ScreenSafe.jsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const SafeScreen = ({ children }) => {
+    const insets = useSafeAreaInsets()
+    return (
+        <View style={{paddingTop: insets.top, flex:1, backgroundColor: 'white'}}>
+            {children}
+        </View>
+    );
+};
+
+export default SafeScreen;  
