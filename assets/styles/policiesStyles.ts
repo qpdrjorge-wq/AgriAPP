bdc0ee6799e526ec6e67ef8ec93c3355a5d6062d
Sir Tapos <qpdrjorge@tip.edu.ph>
2025-10-27 21:44:08 +0800

    Update policiesStyles.ts

--- assets/styles/policiesStyles.ts
+++ assets/styles/policiesStyles.ts
@@ -25,5 +25,58 @@ export const policiesStyles = StyleSheet.create({
         marginVertical: 15,
         marginTop: 25,
     },
+
+    blackCard:{
+        height: 3350,
+        width: '95%',
+        backgroundColor: '#0b0b0bff',
+        alignSelf: 'center',
+        borderRadius: 12,
+    },
+
+    whiteLine: {
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 2,
+        width: "95%",
+        alignSelf: "center",
+        marginTop: 25,
+    },
+
+    redCard: {
+        height: 69,
+        width: '100%',
+        backgroundColor: '#eb383bff',
+        borderRadius: 5,
+        alignSelf: 'stretch',
+        padding: 5,       
+    },
+
+    policyContainer:{
+        margin: 30,
+    },
     
+    policyParty: {
+        fontSize: 28,
+        fontWeight: '700',
+        color: 'white',
+        alignSelf: 'flex-start',
+        marginBottom: 20,
+        marginTop: 30,
+    },
+
+    policySection:{
+        fontSize: 22,
+        fontWeight: '700',
+        color: 'white',
+        alignSelf: 'flex-start',
+
+    },
+
+    policyDetails: {
+        fontSize: 15,
+        color: 'white',
+        lineHeight: 23,
+        marginBottom: 10,
+    }
+
 });
