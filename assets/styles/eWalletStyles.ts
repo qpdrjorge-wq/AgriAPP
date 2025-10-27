ce2390556877f47fa605085ec77d7411176b67f5
Sir Tapos <qpdrjorge@tip.edu.ph>
2025-10-28 03:05:37 +0800

    Add files via upload

--- assets/styles/eWalletStyles.ts
+++ assets/styles/eWalletStyles.ts
@@ -4,7 +4,6 @@ export const eWalletStyles = StyleSheet.create({
     headerText: {
         fontSize: 28,
         fontWeight: 'bold',
-        width: '100%',
         color: 'black',
         marginLeft: 20,
         marginTop: 17,
@@ -23,7 +22,130 @@ export const eWalletStyles = StyleSheet.create({
         width: "90%",
         alignSelf: "center",
         marginVertical: 15,
-        marginTop: 25,
+    },
+
+    linkAccText: {
+        fontSize: 22,
+        fontWeight: '800',
+        color: 'black',
+        marginLeft: 28,
+        marginTop: 15, 
+    }, 
+
+    gcashCard:{
+        height: 100,
+        width: '85%',
+        backgroundColor: '#2283E9',
+        alignSelf: 'center',
+        borderRadius: 10,
+        marginTop: 15,
+        marginBottom: 8,
+        elevation: 3,
+        padding: 23,
+    },
+
+    eWalletLogo: {
+        height: 41,
+        width: 174,
+    },
+
+    addButt: {
+        height: 33,
+        width: 33,
+        marginLeft: 80,
     },
     
+    container: {
+        flexDirection: 'row',
+        marginLeft: 10,
+        marginTop: 5,
+        alignItems: 'center', 
+    },
+
+    payMayaCard:{
+        height: 100,
+        width: '85%',
+        backgroundColor: '#FFFFFF',
+        alignSelf: 'center',
+        borderRadius: 10,
+        marginTop: 15,
+        marginBottom: 8,
+        elevation: 3,
+        padding: 23,
+    },
+
+    blueAddButtIMG: {
+        height: 33,
+        width: 33,
+        marginLeft: 80,
+    },
+
+    modalOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    modalBox: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        width: '100%',
+    },
+
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+
+    buttonRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 15,
+    },
+
+    cancelBtn: {
+        padding: 10,
+        backgroundColor: '#ccc',
+        borderRadius: 5,
+        flex: 1,
+        marginRight: 5,
+        alignItems: 'center',
+    },
+
+    linkButt: {
+        padding: 10,
+        backgroundColor: '#2350d6ff',
+        borderRadius: 5,
+        flex: 1,
+        marginLeft: 5,
+        alignItems: 'center',
+    },
+
+    linkText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+
+    successTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#169e1cff',
+    },
+
+    successIMG:{
+        alignSelf: 'center'
+    }
 });
